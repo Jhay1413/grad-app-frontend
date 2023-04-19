04f6a11590f039ccec26e3baa9454625b0ec533e
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-04-19 13:51:00 +0800

    Latest Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
+
 import AdminLayout from './components/admin/admin-components/adminLayout';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter,Route,Routes,Link } from 'react-router-dom';
 
 function App() {
   return (
