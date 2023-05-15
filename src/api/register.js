520cbffaed273d6dd4790c91128967e2ee853297
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-05-15 11:08:35 +0800

    new updates in headers

--- src/api/register.js
+++ src/api/register.js
@@ -5,13 +5,10 @@ const Users_API = process.env.REACT_APP_USER_API;
 export const addUsers = async (data) =>{
   
     try {
-        const response = await axios.post(Users_API, data, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+        const response = await axios.post(`${Users_API}`,data);
         return response;
-      } catch (error) {
+    } catch (error) {
         return error;
-      }
-};
+    }
+  
+}
